c3dcddb5ea3d1df807685d2b1fc7687fa4463bd7
shist <125158082@qq.com>
2024-04-08 18:15:57 +0800

    change redirect

--- my-app/auth.config.ts
+++ my-app/auth.config.ts
@@ -7,8 +7,9 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/components');
-      if (isOnDashboard) {
+      console.log(`auth.config nexturl:${nextUrl.pathname}`)
+      const notLoginPage = !nextUrl.pathname.startsWith('/login');
+      if (notLoginPage) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
